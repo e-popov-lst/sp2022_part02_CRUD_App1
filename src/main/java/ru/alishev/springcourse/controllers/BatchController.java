c7980c1fe09e0d474083346037caa563541ac11f
Eugene Popov <eugene.popov@list.ru>
2023-05-02 01:33:57 +0300

    lesson 6-1

--- src/main/java/ru/alishev/springcourse/controllers/BatchController.java
+++ src/main/java/ru/alishev/springcourse/controllers/BatchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ru.alishev.springcourse.dao.PersonDAO;
 
 @Controller
-@RequestMapping("test-batch-update")
+@RequestMapping("/test-batch-update")
 public class BatchController {
 
     private final PersonDAO personDAO;
